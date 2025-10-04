f56bdef145a530d4e63011469555bb4a8cd6b8b6
kjacobpaul490 <jacobpaul.kuchipudi@gmail.com>
2025-10-04 18:59:43 +0530

    Angular Built-In Pipes (with Examples)

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,11 +4,12 @@ import { UsersList } from '../users/users-list/users-list';
 import { Caluculator } from '../mathfunction/caluculator/caluculator';
 import { PropertyBinding } from '../pages/property-binding/property-binding';
 import { GenderBasedProduct } from '../pages/gender-based-product/gender-based-product';	
+import { BuiltInPipes } from '../pages/built-in-pipes/built-in-pipes';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, 
-    UsersList,Caluculator,PropertyBinding,GenderBasedProduct],
+    UsersList,Caluculator,PropertyBinding,GenderBasedProduct,BuiltInPipes],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
---
+++ src/pages/built-in-pipes/built-in-pipes.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { dateTimestampProvider } from 'rxjs/internal/scheduler/dateTimestampProvider';
+
+@Component({
+  selector: 'app-built-in-pipes',
+  imports: [CommonModule],
+  templateUrl: './built-in-pipes.html',
+  styleUrl: './built-in-pipes.css'
+})
+export class BuiltInPipes {
+  today: Date = new Date();
+  gender = 'male';
+  inviteMap = {
+    male: 'Invite him',
+    female: 'Invite her',
+    other: 'Invite them'
+  };
+  messages = ['Hi', 'Hello','hiee','byee'];
+  messageMap = {
+    '=0': 'No messages',
+    '=1': '1 message',
+    '=2': '# bugs',
+    'other': '# messages'
+  };
+}
+
+
